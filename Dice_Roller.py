f5ad83024f7d40de58689d05a22da79cc7773809
John Ixcoy <jixcoy316@gmail.com>
2022-06-13 15:23:49 -0700

    added code lol

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -7,3 +7,19 @@ Basic dice roller game using Python.
 
 Libraries: random
 """
+import random
+
+
+print("Welcome to Dice Roller\n\n")
+
+user_name = input("Enter your name: ")
+running = True
+
+while running:
+    start = input("Type \"Enter\" to roll or \"Quit\" to stop playing.\n")
+    if start.lower() == "quit":
+        running = False
+
+    roll = random.randint(1, 9)
+    print(f"Roll Result: {roll}")
+    print("\n===========================\n")
