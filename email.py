cd7d64b2965b8203d4f3c623cac361f98532d711
John Ixcoy <jixcoy316@gmail.com>
2022-06-22 01:15:32 -0700

    New Poject initial commit

---
+++ email.py
@@ -0,0 +1,14 @@
+"""
+Programmers: John Ixcoy     Date: 6/22/2022
+Project Name: Email Sender
+
+Description:
+Python script that sends emails every day to my girlfriend saying "love you twat".
+
+Resources:
+Email sending tutorial
+https://realpython.com/python-send-email/
+
+Libraries:
+smtplib - python builtin
+"""
