4c293b54d759386908d8c11cb894ccdae8ee882d
John Ixcoy <jixcoy316@gmail.com>
2022-07-12 15:18:18 -0700

    Added all properties and constuctor for Car class. Rename

---
+++ CSharpPractice/CSharpPractice/CarClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace CSharpPractice
+{
+    internal class CarTestClass
+    {
+        private int doors;
+
+        public int Doors
+        {
+            get { return doors; }
+            set
+            {
+                if ((value > 4)) { doors = 4; }
+                else { doors = value; }
+            }
+        }
+        public int Wheels { get; set; }
+
+    }
+}
--- CSharpPractice/CSharpPractice/Class1.cs
+++ CSharpPractice/CSharpPractice/Class1.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-
 namespace CSharpPractice
 {
     internal class CarTestClass
     {
+        /*  This class will provide the following information:
+            - how many doors are in the car
+            - how many wheels the car has
+            - how many passengers can the car hold (including the driver)
+            - what color the car is
+         */
         private int doors;
+        private int passengers;
+        private int wheels;
 
+        public CarTestClass(int doors, int passengers, int wheels, string color)
+        {
+            this.Doors = doors;
+            this.Passengers = passengers;
+            this.Wheels = wheels;
+            this.Color = color;
+        }
         public int Doors
         {
             get { return doors; }
@@ -16,7 +30,24 @@ namespace CSharpPractice
                 else { doors = value; }
             }
         }
-        public int Wheels { get; set; }
-
+        public int Passengers
+        {
+            get { return passengers; }
+            set
+            {
+                if (value != doors) { passengers = doors; }
+                else { passengers = value; }
+            }
+        }
+        public int Wheels
+        {
+            get { return wheels; }
+            set
+            {
+                if (value % 2 != 0) { wheels = 4; }
+                else { wheels = value; }
+            }   
+        }
+        public string Color { get; set; }
     }
 }
